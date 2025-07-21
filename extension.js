ee60ef469f8abddd2019cf5b1b31c17a87e08db3
Jussi <saila.jussi@gmail.com>
2025-07-21 12:45:31 +0300

    Register extension as formatting provider instead of normal command.

--- extension.js
+++ extension.js
@@ -10,40 +10,31 @@ const CobolFormatter = require("./src/CobolFormatter");
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    const disposable = vscode.commands.registerCommand(
-        "kopo-formatter.formatDocument",
-        () => {
-            const { activeTextEditor } = vscode.window;
-            if (
-                activeTextEditor &&
-                activeTextEditor.document.languageId === "cobol"
-            ) {
+    const disposable = vscode.languages.registerDocumentFormattingEditProvider(
+        "cobol",
+        {
+            provideDocumentFormattingEdits(document) {
                 const settings =
                     vscode.workspace.getConfiguration("cobol-formatter");
+
                 const formatter = new CobolFormatter({
-                    indentationSpaces: settings.get("indentationSpaces") || 3,
-                    addEmptyLineAfterExit:
-                        settings.get("addEmptyLineAfterExit") || true,
-                    evaluateIndentWhen:
-                        settings.get("evaluateIndentWhen") || true,
-                    alignPicClauses: settings.get("alignPicClauses") || true,
+                    indentationSpaces: settings.get("indentationSpaces"),
+                    addEmptyLineAfterExit: settings.get(
+                        "addEmptyLineAfterExit"
+                    ),
+                    evaluateIndentWhen: settings.get("evaluateIndentWhen"),
+                    alignPicClauses: settings.get("alignPicClauses"),
                 });
 
-                const text = activeTextEditor.document.getText();
+                const text = document.getText();
                 const formattedText = formatter.format(text);
-
-                const edit = new vscode.WorkspaceEdit();
                 const wholeDocument = new vscode.Range(
-                    activeTextEditor.document.positionAt(0),
-                    activeTextEditor.document.positionAt(text.length)
+                    document.positionAt(0),
+                    document.positionAt(text.length)
                 );
-                edit.replace(
-                    activeTextEditor.document.uri,
-                    wholeDocument,
-                    formattedText
-                );
-                vscode.workspace.applyEdit(edit);
-            }
+
+                return [vscode.TextEdit.replace(wholeDocument, formattedText)];
+            },
         }
     );
 
